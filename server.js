7b8653fcfb422d8c939decb9c839ceedc7b87ed2
EliteHuskie <stratton_10@hotmail.com>
2023-07-11 19:38:25 -0300

    Adding logic for GET and POST, added comments to file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Dependencies for application
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -9,6 +10,7 @@ app.use(express.static('public'));
 
 const notesFilePath = path.join(__dirname, 'db', 'db.json');
 
+// Get Notes - Callback
 const getNotesFromFile = (callback) => {
   fs.readFile(notesFilePath, 'utf8', (err, data) => {
     if (err) {
@@ -21,6 +23,7 @@ const getNotesFromFile = (callback) => {
   });
 };
 
+// Save Notes - Callback
 const saveNotesToFile = (notes, callback) => {
   fs.writeFile(notesFilePath, JSON.stringify(notes), (err) => {
     if (err) {
@@ -31,3 +34,30 @@ const saveNotesToFile = (notes, callback) => {
     }
   });
 };
+
+// GET /api/notes - Fetches all notes
+app.get('/api/notes', (req, res) => {
+  getNotesFromFile((notes) => {
+    res.json(notes);
+  });
+});
+
+// POST /api/notes - Creates new note
+app.post('/api/notes', (req, res) => {
+  const newNote = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+
+  getNotesFromFile((notes) => {
+    notes.push(newNote);
+    saveNotesToFile(notes, (err) => {
+      if (err) {
+        res.status(500).json({ error: 'Server error' });
+      } else {
+        res.status(201).json({ success: true });
+      }
+    });
+  });
+});
